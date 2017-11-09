c729366a3a8fec795f4d85c359c629653dffa65f
Jim Fisher <jameshfisher@gmail.com>
2017-11-09 15:10:05 +0000

    example

--- example.php
+++ example.php
@@ -1,4 +1,14 @@
 <?php
 include 'src/PushNotifications.php';
-$pushNotifications = new Pusher\PushNotifications();
-$pushNotifications->publish();
+$pushNotifications = new Pusher\PushNotifications(array(
+  "instanceId" => "YOUR_INSTANCE_ID_HERE",
+  "secretKey" => "YOUR_SECRET_HERE",
+  "endpoint" => "https://errol-server-production.herokuapp.com",
+));
+$publishResponse = $pushNotifications->publish(array(
+  "interests" => array("donuts"),
+  "apns" => array("aps" => array(
+    "alert" => "Hello!",
+  )),
+));
+echo("Published with Publish ID: " . $publishResponse->publishId . "\n");
